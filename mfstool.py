b9777be4eb19ddc82f7eeadfba5cc8a98daa805c
Nico <nico.nap@outlook.com>
2023-05-15 13:51:58 +0200

    Implement list command

--- mfstool.py
+++ mfstool.py
@@ -50,6 +50,27 @@ def parse_superblock(sbdata):
     return sbdict
 
 
+def listdir(diskimg, sbdict):
+    '''
+    This function lists all the directories and files in the root directory.
+
+    Parameters:
+        diskimg (file): The file of the disk image.
+        sbdict (dict): The dictionary with all the data from the superblock.
+    '''
+    # The first data block (root) can be found at first_data_zone.
+    f.seek(BLOCK_SIZE * sbdict["first_data"], 0)
+    root_data = f.read(BLOCK_SIZE)
+
+    # Iterate over root_data and print each directory name.
+    for i in range(2, len(root_data), 16):
+        (name,) = struct.unpack("<14s", root_data[i: i + 14])
+        stripped = name.rstrip(b"\0")
+        if len(stripped) != 0:
+            sys.stdout.buffer.write(stripped)
+            sys.stdout.buffer.write(b'\n')
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: mfstool.py image command params")
@@ -59,12 +80,12 @@ if __name__ == "__main__":
     cmd = sys.argv[2]
 
     with open(diskimg, "rb") as f:
-        #...
-
         # Skip boot block
         f.seek(BLOCK_SIZE, 0)
         # Read super block
         sbdata = f.read(BLOCK_SIZE)
 
         sbdict = parse_superblock(sbdata)
-        print(sbdict)
+
+        if cmd == 'ls':
+            listdir(f, sbdict)
